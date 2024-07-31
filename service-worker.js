ca3a668e7a2d376b89edb097ccae48c1b86ca934
ED0LEO <86008935+ED0LEO@users.noreply.github.com>
2024-07-31 03:10:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,14 @@
 self.addEventListener('install', function(event) {
     console.log('Service Worker installing.');
+    // Perform install steps
 });
 
 self.addEventListener('fetch', function(event) {
     console.log('Fetching:', event.request.url);
-    event.respondWith(fetch(event.request));
+    // Respond with the cached resource or fetch from the network
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
