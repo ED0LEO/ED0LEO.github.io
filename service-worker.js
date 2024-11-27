61c6060e96d9185ff7e1df4a844fd91033699b83
ED0LEO <86008935+ED0LEO@users.noreply.github.com>
2024-11-27 02:12:41 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,13 @@ self.addEventListener('install', (event) => {
         '/',
         '/tracker.html',
         '/styles.css',
-        '/script.js'
+        '/script.js',
+        '/android/android-launchericon-512-512.png',
+        '/android/android-launchericon-192-192.png',
+        '/android/android-launchericon-144-144.png',
+        '/android/android-launchericon-96-96.png',
+        '/android/android-launchericon-72-72.png',
+        '/android/android-launchericon-48-48.png'
       ]);
     })
   );
